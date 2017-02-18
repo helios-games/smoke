01a35cf851d9b4a040763f851d4f814edab7ff24
Alex Collins <alex.e.c@gmail.com>
2017-02-18 19:24:41 +0000

    fixed

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -eux
 
-if [ -e ~/.m2/repository/web-lang/web-lang/1.0.0-SNAPSHOT/web-lang-1.0.0-SNAPSHOT-jar-with-dependencies.jar ]; then
+if [ -e ~/.m2/repository/web-lang/web-lang/1.0.0-SNAPSHOT/web-lang-1.0.0-SNAPSHOT-jar-with-dependencies.jar1 ]; then
   exit
 fi
 
 if [ ! -e web-lang ]; then
   git clone https://github.com/alexec/web-lang.git
 fi
-mvn -f web-lang install -DskipTests -Dskip
+mvn -f web-lang install -DskipTests
 # rm -Rf web-lang
