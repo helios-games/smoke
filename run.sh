948ba29eecee88061417b977cfac98e8861e60a5
Alex Collins <alex.e.c@gmail.com>
2017-02-19 20:57:59 +0000

    roulette

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 set -eux
 
-java -Dwl.path=journeys -Dwl.browser=chrome -Dwebdriver.chrome.driver=web-lang/bin/mac64/chromedriver -jar ~/.m2/repository/web-lang/web-lang/1.0.0-SNAPSHOT/web-lang-1.0.0-SNAPSHOT-jar-with-dependencies.jar
+ARCH=$([ $(uname) = "Darwin" ] && echo "mac64" || echo "linux64")
+
+java -Dwl.path=journeys -Dwl.browser=chrome -Dwebdriver.chrome.driver=web-lang/bin/$ARCH/chromedriver -jar ~/.m2/repository/web-lang/web-lang/1.0.0-SNAPSHOT/web-lang-1.0.0-SNAPSHOT-jar-with-dependencies.jar
