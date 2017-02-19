657f526f1df4d4bef0a21ab089bf05eb0d17fa72
Alex Collins <alex.e.c@gmail.com>
2017-02-19 19:17:00 +0000

    chrome

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 set -eux
 
-java -Dwl.path=journeys -jar ~/.m2/repository/web-lang/web-lang/1.0.0-SNAPSHOT/web-lang-1.0.0-SNAPSHOT-jar-with-dependencies.jar
+java -Dwl.path=journeys -Dwl.browser=chrome -Dwebdriver.chrome.driver=web-lang/bin/mac64/chromedriver -jar ~/.m2/repository/web-lang/web-lang/1.0.0-SNAPSHOT/web-lang-1.0.0-SNAPSHOT-jar-with-dependencies.jar
