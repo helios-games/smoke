467930529592c3a0c96c9e3ce3c16e9935dad073
Alex Collins <alex.e.c@gmail.com>
2017-02-21 18:19:10 +0000

    updated

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ set -eux
 
 ARCH=$([ $(uname) = "Darwin" ] && echo "mac64" || echo "linux64")
 
-java -Dwl.path=journeys -Dwl.browser=chrome -Dwebdriver.chrome.driver=web-lang/bin/$ARCH/chromedriver -jar ~/.m2/repository/web-lang/web-lang/1.0.0-SNAPSHOT/web-lang-1.0.0-SNAPSHOT-jar-with-dependencies.jar
+java -Dwl.path=journeys -jar ~/.m2/repository/web-lang/web-lang/1.0.0-SNAPSHOT/web-lang-1.0.0-SNAPSHOT-jar-with-dependencies.jar
