ab2bf813e8c89558228ae396ecb3be3b2fbcd7cb
Alex Collins <alex.e.c@gmail.com>
2017-02-23 21:33:55 +0000

    fixed

--- test/specs/games/roulette.js
+++ test/specs/games/roulette.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 describe('Roulette', function() {
   it('should be as expected', function () {
     browser.url('http://games.alexecollins.com/games/roulette');
-    expect(browser.getTitle()).to.contain("Classic Slot");
+    expect(browser.getTitle()).to.contain("Roulette");
     expect($('#balance').getText()).to.match(/^[.0-9]+$/);
   });
 });
