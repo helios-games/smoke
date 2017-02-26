67578b41025c53b53e9b83cdda11af133cf198f9
Alex Collins <alex.e.c@gmail.com>
2017-02-26 19:38:47 +0000

    phoebus

--- test/specs/index.js
+++ test/specs/index.js
@@ -3,6 +3,6 @@ var expect = require('chai').expect;
 describe('Homepage', function() {
   it('should be as expected', function () {
     browser.url('http://games.alexecollins.com');
-    expect(browser.getTitle()).to.contain("Welcome to Helios Games");
+    expect(browser.getTitle()).to.contain("Welcome to Phoebus Games");
   });
 });
