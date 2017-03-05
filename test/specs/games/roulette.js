eab11d7b8b5f9d81127eaf63167994a6e4f4f69e
Alex Collins <alex.e.c@gmail.com>
2017-03-05 19:19:48 +0000

    homepage

--- test/specs/games/roulette.js
+++ test/specs/games/roulette.js
@@ -2,6 +2,7 @@ var expect = require('chai').expect;
 
 describe('Roulette', function() {
   it('should be as expected', function () {
+    browser.url('http://games.alexecollins.com');
     browser.url('http://games.alexecollins.com/games/roulette');
     expect(browser.getTitle()).to.contain("Roulette");
     expect($('#balance').getText()).to.match(/^[.0-9]+$/);
