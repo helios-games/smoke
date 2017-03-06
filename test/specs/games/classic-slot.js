98ce89cdbeb8b5943ba82441073b3dbe7be11938
Alex Collins <alex.e.c@gmail.com>
2017-03-06 23:59:00 +0000

    updated test

--- test/specs/games/classic-slot.js
+++ test/specs/games/classic-slot.js
@@ -2,6 +2,7 @@ var expect = require('chai').expect;
 
 describe('Classic Slot', function() {
   it('should be as expected', function () {
+    browser.url('http://games.alexecollins.com');
     browser.url('http://games.alexecollins.com/games/classic-slot');
     expect(browser.getTitle()).to.contain("Classic Slot");
     expect($('#balance').getText()).to.match(/^[.0-9]+$/);
